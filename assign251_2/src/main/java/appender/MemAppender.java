ad173a80ad600d295d3861bbb18e325b2f291195
AdhDyslexie <jonty-mg_work@outlook.com>
2025-10-19 15:54:26 +1300

    Added method mentioned in task 1

--- assign251_2/src/main/java/appender/MemAppender.java
+++ assign251_2/src/main/java/appender/MemAppender.java
@@ -7,17 +7,17 @@ import org.apache.logging.log4j.core.ErrorHandler;
 import org.apache.logging.log4j.core.Layout;
 import org.apache.logging.log4j.core.LogEvent;
  
-// import java.util.ArrayList;
-// import java.util.Collections;
+import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Collections;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
 
 public class MemAppender extends AbstractAppender
 {
-    private static int maxSize;
+    private static int maxSize = 100;
+    private static long discardedLogsCount = 0;
     private static List<LogEvent> logEvents;
+    private static Layout<? extends Serializable> layout;
     
     private static MemAppender instance = new MemAppender();
 
@@ -31,7 +31,6 @@ public class MemAppender extends AbstractAppender
             throw new IllegalCallerException("MemAppender instance already exists!");
         }
     }
-
     
     public static void setMaxSize(int maxSize) {
         MemAppender.maxSize = maxSize;
@@ -39,6 +38,18 @@ public class MemAppender extends AbstractAppender
 
     public static int getMaxSize() {
         return maxSize;
+    }
+
+    public static long getDiscardedLogsCount() {
+        return discardedLogsCount;
+    }
+    
+    private static void setDiscardedLogsCount(long newCount) {
+        MemAppender.discardedLogsCount = newCount;
+    }
+
+    public static void resetDiscardedLogsCount() {
+        setDiscardedLogsCount(0);
     }
 
     public static void setLogEvents(List<LogEvent> logEvents) {
@@ -51,27 +62,45 @@ public class MemAppender extends AbstractAppender
 
 
     public static void addLogEvent(LogEvent logEvent) {
-        if(logEvents.size() < maxSize) {
-            logEvents.add(logEvent);
-        } else {
-            // Remove oldest log event
+        if(logEvents.size() >= maxSize) {
+            // Remove oldest log event and count as discarded
             logEvents.remove(0);
-            logEvents.add(logEvent);
+            discardedLogsCount++;
         }
+        logEvents.add(logEvent);
     }
 
-    public static void getCurrentLogs() {}
-    public static void getEventStrings() {}
-    public static void printLogs() {}
-    public static void setLayout() {}
+    // Not sure if needs to check for null List
+    public static List<LogEvent> getCurrentLogs() {
+        return Collections.unmodifiableList(logEvents);
+    }
 
+    public static List<String> getEventStrings() throws IllegalStateException {
+        if (layout == null) {
+            throw new IllegalStateException("Layout is not set");
+        }
+        List<String> eventStrings = new ArrayList<>();
+        for (LogEvent event : logEvents) {
+            eventStrings.add(layout.toSerializable(event).toString());
+        }
+        return Collections.unmodifiableList(eventStrings);
+    }
 
+    public static void printLogs() {
+        for (LogEvent event : logEvents) {
+            System.out.println(layout.toSerializable(event).toString());
+        }
+        logEvents.clear();
+    }
+    
+    public static void setLayout(Layout<? extends Serializable> layout) {
+        MemAppender.layout = layout;
+    }
 
 
     @Override
     public void append(LogEvent event) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'append'");
+        addLogEvent(event);
     }
 
 
