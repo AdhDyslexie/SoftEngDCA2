2abf3ae626bb63221047e57fed146715b9daefd0
AdhDyslexie <jonty-mg_work@outlook.com>
2025-10-19 13:31:16 +1300

    Added test for memAppender

---
+++ assign251_2/src/test/java/appender/TestMemAppender.java
@@ -0,0 +1,22 @@
+package appender;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestMemAppender 
+{
+
+    @Test
+    public void testSingleton()
+    {
+        MemAppender instance1 = MemAppender.getInstance();
+        MemAppender instance2 = MemAppender.getInstance();
+        assertSame(instance1, instance2);
+    }
+
+
+}
